4795f74171567d16498dfa93c6d13ee85996187a
Gabriella Suarez <g.suarez@correo.unimet.edu.ve>
2023-03-23 18:47:37 -0400

    administrador pero ahora si

--- ProyectoSO2_BS/src/ProyectoSO2/Clases/Administrador.java
+++ ProyectoSO2_BS/src/ProyectoSO2/Clases/Administrador.java
@@ -22,7 +22,9 @@ public class Administrador {
     Cola nivel1TLOU;
     Cola nivel2TLOU;
     Cola nivel3TLOU; 
-    //Crear aquí las 3 colas de prioridad de RM, pueden ser nivel1RM, nivel2RM y nivel3RM
+    ColaRyM nivel1RM;
+    ColaRyM nivel2RM;
+    ColaRyM nivel3RM; 
     Cola refuerzo;
     
     public Administrador() throws IOException{
@@ -33,7 +35,9 @@ public class Administrador {
     nivel2TLOU = new Cola();
     nivel3TLOU = new Cola();
     refuerzo = new Cola();
-    //Crear aquí las colitas de RM
+    nivel1RM = new ColaRyM(); 
+    nivel2RM = new ColaRyM();
+    nivel3RM = new ColaRyM();
     api = new API();
     
     
@@ -58,11 +62,25 @@ public class Administrador {
         return idTLOU;
     }
     
-    //Craer los lindos metodos de RM
+     public ColaRyM getNivel1RM() {
+        return nivel1RM;
+    }
+
+    public ColaRyM getNivel2RM() {
+        return nivel2RM;
+    }
+
+    public ColaRyM getNivel3RM() {
+        return nivel3RM;
+    }
+
+    public int getIdRM() {
+        return idRM;
+    }
     
+   
     
-    
-    //Guiate de este miuntosTLOU y prioridadTLOU para el extra de lo aleatorio de tu serie.
+
     public int minutosTLOU(){
         int intro = 0;
         int inicio1 = 0;
@@ -92,6 +110,27 @@ public class Administrador {
         return minutos;
     
     }
+    
+     public int minutosRM(){
+        int introRM = 0; 
+        int inicioRM1 = 0;
+        int inicioRM2 = 0;
+        int cierreRM = 0;
+        int creditosRM = 0;
+        int prob_calidadRM = 0;
+        int minutos =0;
+        
+        // RM tiene 1 intro, dos inicios, 1 cierre, 1 credito 
+        introRM = new Random().nextInt(100)+1; //1 Intro = 0-100        
+        inicioRM1 = new Random().nextInt(100)+1; //2 Inicios = 0-200
+        inicioRM2 = new Random().nextInt(100)+1;
+        cierreRM = new Random().nextInt(100)+1; //2 Cierres = 0-200       
+        creditosRM = new Random().nextInt(100)+1; //1 Credito = 0-100      
+        prob_calidadRM = (introRM+inicioRM1+inicioRM2+cierreRM+creditosRM)/6;        
+        minutos = 10+prob_calidadRM;       
+        return minutos;   
+    }
+     
     public int PrioridadTLOU(){
         int minutos = minutosTLOU();
         int prioridad =0;
@@ -112,6 +151,24 @@ public class Administrador {
         }
         return prioridad;
     
+    }
+    
+    public int PrioridadRM(){
+        int minutos = minutosRM();
+        int prioridad =0;     
+        if (minutos <60){
+             //Prioridad mas baja
+             prioridad = 3;
+         }       
+        else if (minutos <90 ){
+            //Prioridad media
+            prioridad = 2;
+        }  
+        else if (minutos >= 90 ){
+            //Prioridad alta
+            prioridad = 1;
+        }
+        return prioridad; 
     }
     
     //Metodo para agregar una serie a la cola que le corresponde
